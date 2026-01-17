7faa10e36349c53921c3c67f222dac3c3d216d4d
Denis Carvalho <47404461+deenisdee@users.noreply.github.com>
2026-01-17 13:56:09 -0300

    Update create-preference.js

--- api/create-preference.js
+++ api/create-preference.js
@@ -11,7 +11,7 @@ module.exports = async (req, res) => {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   } 
-
+ 
   try {
     const { plan, email } = req.body;
      
