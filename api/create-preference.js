42c81a811c39d926d1e7f82c994f6c88c86a6397
Denis Carvalho <47404461+deenisdee@users.noreply.github.com>
2026-01-17 14:58:11 -0300

    Update create-preference.js

--- api/create-preference.js
+++ api/create-preference.js
@@ -16,8 +16,8 @@ module.exports = async (req, res) => {
     const { plan, email } = req.body;
      
     const prices = {
-      'premium-monthly': 1,
-      'premium-annual': 1
+      'premium-monthly': 37,
+      'premium-annual': 37
     };
     
     const price = prices[plan] || 37;
